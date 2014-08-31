47c46224157af26566f8b671d14d216153eb4a3a
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-08-31 20:30:42 +0400

    Update documentation

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['heliotrope'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='type' href='heliotrope/type.SortOrder.html' title='heliotrope::SortOrder'>SortOrder</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='heliotrope/struct.SortClause.html' title='heliotrope::SortClause'>SortClause</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
