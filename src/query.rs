73932a0f64ffa3321943259a5a4173096f1414c7
Valentin Vasilyev <valentin.vasilyev@outlook.com>
2014-08-31 10:05:17 +0400

    SolrQuery.set_fields implementation + tests

--- src/query.rs
+++ src/query.rs
@@ -14,7 +14,7 @@ impl SolrQuery {
         SolrQuery{query: query.to_string(), fields: None, sorts: None}
     }
 
-    // Adds field (fl) to the list of returned fields
+    /// Adds field (fl) to the list of returned fields
     pub fn add_field(&self, field: &str) -> SolrQuery {
         let mut fields = self.fields.clone();
         fields = match fields {
@@ -25,6 +25,14 @@ impl SolrQuery {
             None => Some(vec!(field.to_string()))
         };
         SolrQuery{query: self.query.clone(), fields: fields, sorts: self.sorts.clone()}
+    }
+
+    /// Sets fields (fl) as the list of returned fields.
+    /// The already set fields are overwritten.
+    pub fn set_fields(&self, fields: &[&str]) -> SolrQuery {
+        let mut new_fields = Vec::with_capacity(fields.len());
+        new_fields.extend(fields.iter().map(|x| x.to_string()));
+        SolrQuery{query:self.query.clone(), fields: Some(new_fields), sorts:self.sorts.clone()}
     }
 
     /// Converts this query to a vector of pairs, suitable for URL percent encoding
--- tests/solr_query_tests.rs
+++ tests/solr_query_tests.rs
@@ -10,12 +10,33 @@ fn query_only_query_to_pairs() {
 }
 
 #[test]
-fn query_and_field_to_pairs() {
+fn query_and_field_with_add_field_to_pairs() {
     let mut query = SolrQuery::new("abba");
     query = query.add_field("score");
     assert_eq!(query.to_pairs(),
                vec!(("wt".to_string(), "json".to_string()),
                     ("q".to_string(), "abba".to_string()),
                     ("fl".to_string(), "score".to_string())));
+}
+
+#[test]
+fn query_and_many_fields_with_add_field_to_pairs() {
+    let mut query = SolrQuery::new("abba");
+    query = query.add_field("*");
+    query = query.add_field("score");
+    assert_eq!(query.to_pairs(),
+               vec!(("wt".to_string(), "json".to_string()),
+                    ("q".to_string(), "abba".to_string()),
+                    ("fl".to_string(), "*, score".to_string())));
+}
+
+#[test]
+fn query_and_many_fields_with_set_fields_to_paris() {
+    let mut query = SolrQuery::new("abba");
+    query = query.set_fields(&["id", "title"]);
+    assert_eq!(query.to_pairs(),
+               vec!(("wt".to_string(), "json".to_string()),
+                    ("q".to_string(), "abba".to_string()),
+                    ("fl".to_string(), "id, title".to_string())));
 }
 
