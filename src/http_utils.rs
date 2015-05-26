3476ab18814970d7869873a73626db4b6bce5c92
Dzmitry_Misiuk <minsler.by@gmail.com>
2015-05-26 17:13:46 +0300

    Added handling istead of unwrap() for utils#get

--- src/http_utils.rs
+++ src/http_utils.rs
@@ -15,18 +15,21 @@ pub struct HttpResponse {
 
 pub fn get(url: &Url) -> Result<HttpResponse, Error> {
     let mut client = Client::new();
-    let mut res = client.get(&url.to_string())
-        .send()
-        .unwrap();
-    let mut body = String::new();
-    let result = res.read_to_string(&mut body);
-    match result {
-        Ok(_) => {
-            Ok(HttpResponse{status: res.status, body: body})
+    let mut result_response = client.get(&url.to_string()).send();
+    match result_response {
+        Ok(mut res) => {
+            let mut body = String::new();
+            let result = res.read_to_string(&mut body);
+            match result {
+                Ok(_) => {
+                    Ok(HttpResponse{status: res.status, body: body})
+                },
+                Err(err) => {
+                    Err(Error::Io(err))
+                }
+            }
         },
-        Err(err) => {
-            Err(Error::Io(err))
-        }
+        Err(err) => Err(err)
     }
 }
 
